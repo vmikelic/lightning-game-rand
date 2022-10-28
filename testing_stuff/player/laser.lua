8e5ecd3e7b02478e36e85fd30018be383664525e
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-10-28 17:45:38 -0600

    Added docs

--- testing_stuff/player/laser.lua
+++ testing_stuff/player/laser.lua
@@ -14,15 +14,21 @@ function laser:new(init_x, init_y)
 	return o
 end
 
+-- check for collission against an enemy
 function laser:is_hit()
+
+	-- out of screen bounds
 	if(self.y <= 0) then
 		return true
 	end
+
+	-- check if collided with at least one enemy
 	for en in all(enemy_manager.enemies) do
 		if(self.y+7 > en.y) then
 			if(self.x+3 > en.x) then
 				if(self.y < en.y+8) then
 					if(self.x < en.x+8) then
+						-- destroy enemy and return
 						animate_once(en.x-4,en.y-4,{43,45,64,66,68},2,2,3,1)
 						enemy_manager.remove(en)
 						sfx(3)
